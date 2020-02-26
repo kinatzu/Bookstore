c05d2396f232fe0a0398c6908a5962c07b9d3ad6
[kenneth] <[k.exposito.ubiqum@gmail.com]>
2020-02-26 12:15:31 +0100

    Solve rotation backface

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
-        books:[],
-        search: "",
+        books:[],     //ARRAY TO ADD THE FILTERED BOOKS
+        search: "",   // SEARCH BAR WITH EMPTY STRING
         es: "",
         en:""
     },
     methods: {
 
 
-        getData(){
+        getData(){     //getData () es una función para recuperar datos de la API proporcionada, asignarle una marca de tiempo y también asignar los datos recuperados al almacenamiento local. Una vez que ambos están completos, se llama a la función Mostrar libros.
             
             var data;
 
@@ -27,31 +27,31 @@ var app = new Vue({
 
                 app.books = data.books;
 
-                localStorage.setItem("booksData", JSON.stringify(app.books));
+                // localStorage.setItem("booksData", JSON.stringify(app.books));
 
-                localStorage.setItem("bookTitleData", JSON.stringify(app.bookTitles));
+                // localStorage.setItem("bookTitleData", JSON.stringify(app.bookTitles));
 
-                dataAge = Date.now();
+                // dataAge = Date.now(); 
 
-                localStorage.setItem("Timestamp", dataAge);
+                // localStorage.setItem("Timestamp", dataAge);
 
             }).catch(function (error) {
                 console.log("Request failed: " + error.message);
             });
         },
     },
-    created() {
+    created() {     //Este bloque de código verificará si hay datos en el almacenamiento local de menos de 1,5 minutos de antigüedad. Si es así, esos datos se utilizarán para la función displayBooks. De lo contrario, se utilizará una nueva búsqueda / visualización.
 
 
         var TimestampNow = Date.now();
 
-        if (localStorage.getItem("booksData") && TimestampNow - localStorage.getItem("Timestamp") < 600000) {
+        if (localStorage.getItem("booksData") && TimestampNow - localStorage.getItem("Timestamp") < 90000) {
             this.books = JSON.parse(localStorage.getItem("booksData"));
         } else {
             this.getData();
         }
     },
-    computed: {
+    computed: { //Esta es nuestra funcionalidad de búsqueda. El sitio web solo muestra libros filtrados, y cuando el cuadro de búsqueda está vacío, hay cero resultados filtrados, por lo que vemos todo. Cada vez que se escribe un carácter en el cuadro, lo compara con el título y la descripción de todos los elementos en la API y muestra solo resultados coincidentes.
 
         filteredBooks: function() {
             return this.books.filter((book) => {
@@ -83,3 +83,4 @@ btn.on('click', function(e) {
   e.preventDefault();
   $('html, body').animate({scrollTop:0}, '300');
 });
+
